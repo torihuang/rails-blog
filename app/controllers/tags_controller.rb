c25cf2ea94c061a4acae3f68d33367b73e70ae76
torihuang <victoriahuang20@gmail.com>
2016-04-10 14:59:36 -0500

    Add ability to create tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -18,16 +18,25 @@ class TagsController < ApplicationController
 
   def create
     @item = Object.new
+    @post = Object.new
+    @blog = Object.new
     if params[:blog_id]
       @item = Blog.find_by(id: params[:blog_id])
+      @blog = @item
     elsif params[:post_id]
       @item = Post.find_by(id: params[:post_id])
+      @post = @item
+      @blog = @post.blog
     else
       render 'new'
     end
 
     if @item
-      @tag = @item.tags.create(tag_params)
+      # Checks for uniqueness in tag
+      @tag = Tag.new(tag_params)
+      @tag = Tag.already_exists(@tag) if Tag.already_exists(@tag)
+      @item.tags << @tag
+      redirect_to @tag
     else
       render 'new'
     end
