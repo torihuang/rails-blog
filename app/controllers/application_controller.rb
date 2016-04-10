baddcc2839142ccffdd826ef46297e4ab7fe65cf
torihuang <victoriahuang20@gmail.com>
2016-04-10 15:08:11 -0500

    Change css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
-  helper :all
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
